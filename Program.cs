6f550bf03d3c61f10b9c513beda5f5b15164949c
AdeyBoy123 <groveraden@gmail.com>
2023-09-14 16:07:11 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string to check: ");
+                {
+                string input = Console.ReadLine();
+
+                if (IsPalindrome(input))
+                {
+                    Console.WriteLine("Palindrome");
+                } 
+                else
+                {
+                    Console.WriteLine("Not Palindrome");
+                }
+            }
+        }
+
+        public static bool IsPalindrome(string input)
+        {
+            return input == new string(input.Reverse().ToArray());
+        }
+    }
+}
